16189e2325e0894662b396937182510962f091d3
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-04-16 20:01:36 +0000

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,18 @@ export default function HomePage() {
   return (
     <PageLayout>
       <Container className="mt-3 mb-3">
-        <h3 className="text-center my-3">SiloCityPages</h3>
+        <h3 className="text-center my-3">SiloCityPhaser</h3>
         <Row className="shadow-lg p-3 bg-body rounded">
           <Col lg={8} className="mx-auto">
-            SiloCityPages, developed by SiloCityLabs, is a streamlined framework
-            designed to accelerate the development and deployment of static
-            websites. Leveraging the power of Bootstrap for responsive design,
-            React for component-driven architecture, and Next.js for efficient
-            rendering, SiloCityPages provides a robust foundation for building
-            modern web applications. Optimized for seamless deployment to GitHub
-            Pages, this framework simplifies the process of creating and hosting
-            performant, visually appealing websites, allowing developers to
-            focus on content and functionality rather than complex
+            SiloCityPhaser, developed by SiloCityLabs, is a streamlined
+            framework designed to accelerate the development and deployment of
+            static websites. Leveraging the power of Bootstrap for responsive
+            design, React for component-driven architecture, and Next.js for
+            efficient rendering, SiloCityPhaser provides a robust foundation for
+            building modern web applications. Optimized for seamless deployment
+            to GitHub Pages, this framework simplifies the process of creating
+            and hosting performant, visually appealing websites, allowing
+            developers to focus on content and functionality rather than complex
             configurations.
           </Col>
         </Row>
